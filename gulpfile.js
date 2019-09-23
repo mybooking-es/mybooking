076e2d931295d3cfb5b8e400ac3caddb6a77da9f
Marcel Reig <marcelreig@gmail.com>
2019-09-23 13:40:17 +0200

    css refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,68 @@
 // Defining requirements
-var gulp = require( 'gulp' );
-var plumber = require( 'gulp-plumber' );
-var sass = require( 'gulp-sass' );
-var rename = require( 'gulp-rename' );
-var rimraf = require( 'gulp-rimraf' );
-var sourcemaps = require( 'gulp-sourcemaps' );
-var cleanCSS = require( 'gulp-clean-css' );
-var autoprefixer = require( 'gulp-autoprefixer' );
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sass = require("gulp-sass");
+var rename = require("gulp-rename");
+var rimraf = require("gulp-rimraf");
+var sourcemaps = require("gulp-sourcemaps");
+var cleanCSS = require("gulp-clean-css");
+var autoprefixer = require("gulp-autoprefixer");
 
 // Configuration file to keep your code DRY
-var cfg = require( './gulpconfig.json' );
+var cfg = require("./gulpconfig.json");
 var paths = cfg.paths;
 
 // Run:
 // gulp watch
 // Starts watcher. Watcher runs gulp sass task on changes
-gulp.task( 'watch', function() {
-    gulp.watch( `${paths.sass}/**/*.scss`, gulp.series('styles') );
+gulp.task("watch", function() {
+  gulp.watch(`${paths.sass}/**/*.scss`, gulp.series("styles"));
 });
-
 
 // Run:
 // gulp sass
 // Compiles SCSS files in CSS
-gulp.task( 'sass', function() {
-    var stream = gulp.src( `${paths.sass}/*.scss` )
-        .pipe( sourcemaps.init( { loadMaps: true } ) )
-        .pipe( plumber( {
-            errorHandler: function( err ) {
-                console.log( err );
-                this.emit( 'end' );
-            }
-        } ) )
-        .pipe( sass( { errLogToConsole: true } ) )
-        .pipe( autoprefixer( 'last 2 versions' ) )
-        .pipe( sourcemaps.write( './' ) )
-        .pipe( gulp.dest( paths.css ) );
-    return stream;
+gulp.task("sass", function() {
+  var stream = gulp
+    .src(`${paths.sass}/*.scss`)
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(
+      plumber({
+        errorHandler: function(err) {
+          console.log(err);
+          this.emit("end");
+        }
+      })
+    )
+    .pipe(sass({ errLogToConsole: true }))
+    .pipe(autoprefixer("last 2 versions"))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest(paths.css));
+  return stream;
 });
 
 // Run:
 // gulp minifycss
 // Minify child-theme.css
-gulp.task( 'minifycss', function() {
-  return gulp.src( paths.css + '/child-theme.css' )
-  .pipe( sourcemaps.init( { loadMaps: true } ) )
-    .pipe( cleanCSS( { compatibility: '*' } ) )
-    .pipe( plumber( {
-            errorHandler: function( err ) {
-                console.log( err ) ;
-                this.emit( 'end' );
-            }
-        } ) )
-    .pipe( rename( { suffix: '.min' } ) )
-     .pipe( sourcemaps.write( './' ) )
-    .pipe( gulp.dest( paths.css ) );
+gulp.task("minifycss", function() {
+  return gulp
+    .src(paths.css + "/child-theme.css")
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(cleanCSS({ compatibility: "*" }))
+    .pipe(
+      plumber({
+        errorHandler: function(err) {
+          console.log(err);
+          this.emit("end");
+        }
+      })
+    )
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest(paths.css));
 });
 
 // Run:
 // gulp styles
 // Compile Sass and minify css
-gulp.task( 'styles', gulp.series( 'sass', 'minifycss' ));
+gulp.task("styles", gulp.series("sass", "minifycss"));
--- mybooking-templates/mybooking-plugin-selector-form.php
+++ mybooking-templates/mybooking-plugin-selector-form.php
@@ -1,16 +1,14 @@
   <section class="widget widget_mybooking_rent_engine_selector reservation-step">
-      <div class="landing-form mx-3">
+    <div class="p-3 bg-transparen-white mx-3">
         <form name="search_form" method="get" enctype="application/x-www-form-urlencoded">
           <div class="form-group w-100">
-            <label class="control-label w-100"
-              >Lugar Entrega
+            <label class="control-label w-100">Lugar Entrega <i class="fa custom-icon fa-angle-down"></i>
               <select class="w-100" id="pickup_place" name="pickup_place">
               </select>
             </label>
           </div>
           <div class="form-group w-100">
-            <label class="control-label w-100"
-              >Lugar Devolución
+            <label class="control-label w-100">Lugar Devolución <i class="fa custom-icon fa-angle-down"></i>
               <select class="w-100" id="return_place" name="return_place">
               </select>
             </label>
@@ -19,32 +17,21 @@
           <div class="form-group">
             <div class="form-row">
               <div class="col">
-                <label class="control-label"
-                  >Fecha Entrega
+                <label class="control-label">Fecha Entrega
                   <div class="input-group">
                     <div class="input-group-prepend">
                       <div class="input-group-text">
                         <i class="fa fa-calendar" aria-hidden="true"></i>
                       </div>
                     </div>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="date_from"
-                      name="date_from"
-                    />
-                    <select
-                      class="input-group-field ml-1"
-                      id="time_from"
-                      name="time_from"
-                    >
+                    <input type="text" class="form-control" id="date_from" name="date_from" />
+                    <select class="input-group-field ml-1" id="time_from" name="time_from">
                     </select>
                   </div>
                 </label>
               </div>
               <div class="col">
-                <label class="control-label"
-                  >Fecha Devolución
+                <label class="control-label">Fecha Devolución
                   <div class="input-group">
                     <div class="input-group-prepend">
                       <div class="input-group-text">
@@ -52,17 +39,8 @@
                       </div>
                     </div>
 
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="date_to"
-                      name="date_to"
-                    />
-                    <select
-                      class="input-group-field ml-1"
-                      id="time_to"
-                      name="time_to"
-                    >
+                    <input type="text" class="form-control" id="date_to" name="date_to" />
+                    <select class="input-group-field ml-1" id="time_to" name="time_to">
                     </select>
                   </div>
                 </label>
@@ -70,11 +48,7 @@
             </div>
           </div>
 
-          <input
-            type="submit"
-            class="btn btn-primary float-right"
-            value="Buscar"
-          />
+          <input type="submit" class="btn btn-primary float-right" href="choose_product.html" value="Buscar" />
         </form>
-      </div>  
+      </div>
   </section>
