5172b5f7d493f2744766e302fbf432df5facc5ef
Hector <hacheunomenorca@gmail.com>
2019-09-24 12:37:59 +0200

    Agregada plantilla de Home y su panel de opciones

--- home.php
+++ home.php
@@ -8,25 +8,22 @@
 * @since Understrap Mybooking Child 0.0.1
 */
 
-// Variables del template
-
-
-// Pintamos la cabecera de página
 get_header();
 ?>
 
-<!-- HERO SECTION -->
+
+<!-- SECCION CABECERA --------------------------------------------------------->
 
 <div class="hero-header-container">
   <div class="hero-header-content">
 
-    <!-- PUNCHLINE -->
+    <!-- Eslogan -->
+
     <div class="hero-header-left">
       <div class="aligner">
-        <!-- Titulo -->
-        <?php
-          $titulo_hero = get_option("home_hero_title");
 
+        <!-- Titulo -->
+        <?php $titulo_hero = get_option("home_hero_title");
     	    if ($titulo_hero !== '') { ?>
     	    	<h1><?php echo $titulo_hero ?></h1>
     	    <?php }
@@ -34,10 +31,9 @@ get_header();
     	  		<h1><?php _e("Título genérico",'mybookinges'); ?></h1>
             <!-- TODO: Añadir título genérico final -->
         <?php } ?>
-        <!-- Texto -->
-        <?php
-          $texto_hero = get_option("home_hero_text");
 
+        <!-- Texto -->
+        <?php $texto_hero = get_option("home_hero_text");
     	    if ($texto_hero !== '') { ?>
     	    	<p><?php echo $texto_hero ?></p>
     	    <?php }
@@ -46,90 +42,279 @@ get_header();
             tote bag twee pok pok scenester selfies.",'mybookinges'); ?></p>
             <!-- TODO: Agregar texto genérico final -->
         <?php } ?>
+
       </div>
     </div>
 
-    <!-- WIDGET AREA -->
+    <!-- Widget -->
+
     <div class="hero-header-right">
+
       <?php if ( is_active_sidebar( 'mybooking_home_hero' ) ) : ?>
       	<?php dynamic_sidebar( 'mybooking_home_hero' ); ?>
       <?php endif; ?>
+
     </div>
   </div>
 
   <div class="diagonal-section"></div>
+
 </div>
 
-<div class="flex-block-wrapper">
-  <div class="centered-flex-block mt150">
-    <h1>Overview</h1>
-    <div class="hr"></div>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae magni culpa
-      laudantium at. Pariatur officia maiores earum
-    </p>
-  </div>
-</div>
 
-<div class="icons-wrapper mt100">
-  <div class="icon-panel icon-1">
-    <i class="fas fa-gas-pump"></i><br />
-    <h5>We don't charge deposit</h5>
-  </div>
-  <div class="icon-panel icon-2">
-    <i class="fas fa-wallet"></i><br />
-    <h5>Competitive prices</h5>
-  </div>
-  <div class="icon-panel icon-3">
-    <i class="fas fa-credit-card"></i><br />
-    <h5>Credit card doesn't required</h5>
-  </div>
-  <div class="icon-panel icon-4">
-    <i class="fas fa-users"></i><br />
-    <h5>Extra drivers included</h5>
-  </div>
-</div>
+<!-- SECCION DESTACADA -------------------------------------------------------->
 
-<div class="gradient-section">
-  <div class="centered-flex-block mt150">
-    <h1 class="color-white">How it works</h1>
-    <div class="hr-white"></div>
-    <p class="color-white">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae magni culpa
-      laudantium at. Pariatur officia maiores earum impedit numquam quod cum
-    </p>
+<?php $highlight_ver = get_option("home_highlight_visibilidad");
+if ($highlight_ver !== 1) { ?>
+
+  <!-- Cabecera -->
+
+  <div class="flex-block-wrapper">
+    <div class="centered-flex-block mt150">
+
+      <!-- Titulo -->
+      <?php $titulo_highlight_cabecera = get_option("home_highlight_cabecera_title");
+        if ($titulo_highlight_cabecera !== '') { ?>
+          <h1><?php echo $titulo_highlight_cabecera ?></h1>
+        <?php }
+        else { ?>
+          <h1><?php _e("Título sección",'mybookinges'); ?></h1>
+          <!-- TODO: Añadir título genérico final -->
+      <?php } ?>
+
+      <!-- Texto -->
+      <?php $texto_highlight_cabecera = get_option("home_highlight_cabecera_text");
+        if ($texto_highlight_cabecera !== '') { ?>
+          <p><?php echo $texto_highlight_cabecera ?></p>
+        <?php }
+        else { ?>
+          <p><?php _e("Texto destacado sin Lorem ipsum dolor amet flannel mumblecore air plant iceland hexagon
+          tote bag twee pok pok scenester selfies.",'mybookinges'); ?></p>
+          <!-- TODO: Agregar texto genérico final -->
+      <?php } ?>
+
+    </div>
   </div>
+
+  <!-- Puntos fuertes -->
+
+  <div class="icons-wrapper mt100">
+    <div class="icon-panel icon-1">
+
+      <?php $imagen_punto_uno = get_option("home_punto_uno_image");
+        if ($imagen_punto_uno !== '') { ?>
+          <img src="<?php echo $imagen_punto_uno ?>" alt="">
+        <?php }
+        else { ?>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/punto_1.svg" alt="">
+      <?php } ?>
+
+      <?php $texto_punto_uno = get_option("home_punto_uno_text");
+        if ($texto_punto_uno !== '') { ?>
+          <br>
+          <h5><?php echo $texto_punto_uno ?></h5>
+        <?php }
+        else { ?>
+          <br>
+          <h5><?php _e("We don't charge deposit",'mybookinges'); ?></h5>
+      <?php } ?>
+
+    </div>
+
+    <div class="icon-panel icon-2">
+
+      <?php $imagen_punto_dos = get_option("home_punto_dos_image");
+        if ($imagen_punto_dos !== '') { ?>
+          <img src="<?php echo $imagen_punto_dos ?>" alt="">
+        <?php }
+        else { ?>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/punto_2.svg" alt="">
+      <?php } ?>
+
+      <?php $texto_punto_dos = get_option("home_punto_dos_text");
+        if ($texto_punto_dos !== '') { ?>
+          <br>
+          <h5><?php echo $texto_punto_dos ?></h5>
+        <?php }
+        else { ?>
+          <br>
+          <h5><?php _e("Competitive prices",'mybookinges'); ?></h5>
+      <?php } ?>
+
+    </div>
+
+    <div class="icon-panel icon-3">
+
+      <?php $imagen_punto_tres = get_option("home_punto_tres_image");
+        if ($imagen_punto_tres !== '') { ?>
+          <img src="<?php echo $imagen_punto_tres ?>" alt="">
+        <?php }
+        else { ?>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/punto_3.svg" alt="">
+      <?php } ?>
+
+      <?php $texto_punto_tres = get_option("home_punto_tres_text");
+        if ($texto_punto_tres !== '') { ?>
+          <br>
+          <h5><?php echo $texto_punto_tres ?></h5>
+        <?php }
+        else { ?>
+          <br>
+          <h5><?php _e("Extra drivers included",'mybookinges'); ?></h5>
+      <?php } ?>
+
+    </div>
+  </div>
+  <!-- TODO: Igualar los margins para darle consistencia cuando actue the_content() -->
+
+<?php } ?>
+
+
+<!-- EL CONTENIDO ------------------------------------------------------------->
+
+<div class="content flex-block-wrapper">
   <div class="centered-flex-block">
-    <img src="assets/images/three-cars.png" alt="" />
-  </div>
 
-  <div class="feature">
-    <div class="features__item">
-      <span class="stopa">1</span>
-      <h3>Choose</h3>
-      <hr />
-      <p>
-        Duis finibus odio sit amet nisi dictum et viverra libero semper donec
-      </p>
-    </div>
-    <div class="features__item">
-      <span class="stopa stopa-2">2</span>
-      <h3>Your</h3>
-      <hr />
-      <p>
-        Integer ac rhoncus urna curabitur feugiat dolor id fermentum rutrum ex
-        tellus tristique ante sit amet malesuada justo diam
-      </p>
-    </div>
-    <div class="features__item">
-      <span class="stopa">3</span>
-      <h3>Car</h3>
-      <hr />
-      <p>
-        Duis finibus odio sit amet nisi dictum et viverra libero semper donec
-      </p>
-    </div>
+    <?php while ( have_posts() ) : the_post(); ?>
+      <!-- TODO: Insertar aquí un separador que actue cuando se activa esta condición -->
+      <?php the_content(); ?>
+    <?php endwhile;?>
+
   </div>
 </div>
+
+
+<!-- SECCION CARACTERISTICAS -------------------------------------------------->
+
+<?php $features_ver = get_option("home_features_visibilidad");
+if ($features_ver !== 1) { ?>
+
+  <div class="gradient-section">
+    <div class="centered-flex-block mt150">
+
+      <!-- Titulo -->
+      <?php $titulo_features_cabecera = get_option("home_features_cabecera_title");
+        if ($titulo_features_cabecera !== '') { ?>
+          <h1 class="color-white"><?php echo $titulo_features_cabecera ?></h1>
+        <?php }
+        else { ?>
+          <h1 class="color-white"><?php _e("How it works",'mybookinges'); ?></h1>
+          <!-- TODO: Añadir título genérico final -->
+      <?php } ?>
+
+      <!-- Texto -->
+      <?php $texto_features_cabecera = get_option("home_features_cabecera_text");
+        if ($texto_features_cabecera !== '') { ?>
+          <p class="color-white"><?php echo $texto_features_cabecera ?></p>
+        <?php }
+        else { ?>
+          <p class="color-white"><?php _e("Texto caracteristicas sin Lorem ipsum dolor amet flannel mumblecore air plant iceland hexagon
+          tote bag twee pok pok scenester selfies.",'mybookinges'); ?></p>
+          <!-- TODO: Agregar texto genérico final -->
+      <?php } ?>
+
+    </div>
+
+    <div class="centered-flex-block">
+
+      <?php $imagen_features_cabecera = get_option("home_features_cabecera_image");
+        if ($imagen_features_cabecera !== '') { ?>
+          <img src="<?php echo $imagen_features_cabecera ?>" alt="">
+        <?php }
+        else { ?>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/three-cars.png" alt="">
+      <?php } ?>
+
+    </div>
+
+    <div class="feature">
+      <div class="features__item">
+
+        <span class="stopa">1</span>
+
+        <!-- Titulo -->
+        <?php $titulo_features_uno = get_option("home_features_uno_title");
+          if ($titulo_features_uno !== '') { ?>
+            <h3><?php echo $titulo_features_uno ?></h3>
+          <?php }
+          else { ?>
+            <h3><?php _e("Choose",'mybookinges'); ?></h3>
+            <!-- TODO: Añadir título genérico final -->
+        <?php } ?>
+
+        <hr />
+
+        <!-- Texto -->
+        <?php $texto_features_uno = get_option("home_features_uno_text");
+          if ($texto_features_uno !== '') { ?>
+            <p><?php echo $texto_features_uno ?></p>
+          <?php }
+          else { ?>
+            <p><?php _e("Texto features Duis finibus odio sit amet nisi dictum et viverra libero semper donec.",'mybookinges'); ?></p>
+            <!-- TODO: Agregar texto genérico final -->
+        <?php } ?>
+
+      </div>
+
+      <div class="features__item">
+
+        <span class="stopa stopa-2">2</span>
+
+        <!-- Titulo -->
+        <?php $titulo_features_dos = get_option("home_features_dos_title");
+          if ($titulo_features_dos !== '') { ?>
+            <h3><?php echo $titulo_features_dos ?></h3>
+          <?php }
+          else { ?>
+            <h3><?php _e("Your",'mybookinges'); ?></h3>
+            <!-- TODO: Añadir título genérico final -->
+        <?php } ?>
+
+        <hr />
+
+        <!-- Texto -->
+        <?php $texto_features_dos = get_option("home_features_dos_text");
+          if ($texto_features_dos !== '') { ?>
+            <p><?php echo $texto_features_dos ?></p>
+          <?php }
+          else { ?>
+            <p><?php _e("Texto features Duis finibus odio sit amet nisi dictum et viverra libero semper donec.",'mybookinges'); ?></p>
+            <!-- TODO: Agregar texto genérico final -->
+        <?php } ?>
+
+      </div>
+
+      <div class="features__item">
+
+        <span class="stopa">3</span>
+
+        <!-- Titulo -->
+        <?php $titulo_features_tres = get_option("home_features_tres_title");
+          if ($titulo_features_tres !== '') { ?>
+            <h3><?php echo $titulo_features_tres ?></h3>
+          <?php }
+          else { ?>
+            <h3><?php _e("Choose",'mybookinges'); ?></h3>
+            <!-- TODO: Añadir título genérico final -->
+        <?php } ?>
+
+        <hr />
+
+        <!-- Texto -->
+        <?php $texto_features_tres = get_option("home_features_tres_text");
+          if ($texto_features_tres !== '') { ?>
+            <p><?php echo $texto_features_tres ?></p>
+          <?php }
+          else { ?>
+            <p><?php _e("Texto features Duis finibus odio sit amet nisi dictum et viverra libero semper donec.",'mybookinges'); ?></p>
+            <!-- TODO: Agregar texto genérico final -->
+        <?php } ?>
+
+      </div>
+
+    </div>
+  </div>
+
+<?php } ?>
 
 <?php get_footer();
--- mybooking-options/mybooking-home-options.php
+++ mybooking-options/mybooking-home-options.php
@@ -38,11 +38,54 @@ function mybookinges_registra_opciones_configuracion() {
   add_option("home_hero_title","","","yes");
   add_option("home_hero_text","","","yes");
 
+  add_option("home_higlight_visibilidad","","","yes");
+  add_option("home_highlight_cabecera_title","","","yes");
+  add_option("home_highlight_cabecera_text","","","yes");
+  add_option("home_punto_uno_image","","","yes");
+  add_option("home_punto_uno_text","","","yes");
+  add_option("home_punto_dos_image","","","yes");
+  add_option("home_punto_dos_text","","","yes");
+  add_option("home_punto_tres_image","","","yes");
+  add_option("home_punto_tres_text","","","yes");
+
+  add_option("home_features_visibilidad","","","yes");
+  add_option("home_features_cabecera_title","","","yes");
+  add_option("home_features_cabecera_text","","","yes");
+  add_option("home_features_cabecera_image","","","yes");
+  add_option("home_features_uno_title","","","yes");
+  add_option("home_features_uno_text","","","yes");
+  add_option("home_features_dos_title","","","yes");
+  add_option("home_features_dos_text","","","yes");
+  add_option("home_features_tres_title","","","yes");
+  add_option("home_features_tres_text","","","yes");
+
   // Registro de opciones
   register_setting("opciones_home", "home_hero_title");
   register_setting("opciones_home", "home_hero_text");
 
+  register_setting("opciones_home", "home_highlight_visibilidad");
+  register_setting("opciones_home", "home_highlight_cabecera_title");
+  register_setting("opciones_home", "home_highlight_cabecera_text");
+  register_setting("opciones_home", "home_punto_uno_image");
+  register_setting("opciones_home", "home_punto_uno_text");
+  register_setting("opciones_home", "home_punto_dos_image");
+  register_setting("opciones_home", "home_punto_dos_text");
+  register_setting("opciones_home", "home_punto_tres_image");
+  register_setting("opciones_home", "home_punto_tres_text");
+
+  register_setting("opciones_home", "home_features_visibilidad");
+  register_setting("opciones_home", "home_features_cabecera_title");
+  register_setting("opciones_home", "home_features_cabecera_text");
+  register_setting("opciones_home", "home_features_cabecera_image");
+  register_setting("opciones_home", "home_features_uno_title");
+  register_setting("opciones_home", "home_features_uno_text");
+  register_setting("opciones_home", "home_features_dos_title");
+  register_setting("opciones_home", "home_features_dos_text");
+  register_setting("opciones_home", "home_features_tres_title");
+  register_setting("opciones_home", "home_features_tres_text");
+
 }
+
 
 function mybookinges_configuracion_home() {
   if (!current_user_can('edit_pages'))
@@ -53,38 +96,168 @@ function mybookinges_configuracion_home() {
 
     <!-- Titulo de la página -->
 
-    <h1><span class="dashicons dashicons-admin-generic" style="font-size: 2rem; margin-right: 1rem;"></span> Configuración de portada <small>- Opciones de configuración para la página de inicio</small></h1>
+    <h1>
+      Configuración de la página de inicio <br>
+      <small>Ajustes para el template 'MyBooking Home'</small>
+    </h1>
+
+    <hr>
 
     <?php settings_errors(); ?>
 
-    <!-- Formulario -->
+    <!-- Formulario ----------------------------------------------------------->
 
     <form method="post" action="options.php">
 
       <?php settings_fields('opciones_home'); ?>
 
-      <!-- Seccion Hero -->
-      <h2>Hero</h2>
-      <p>Configura el título y texto por defecto en el top de la página de inicio, junto al formulario de selección de fechas.</p>
-      <hr>
+      <!-- Seccion Cabecera -->
 
-      <h2>Eslogan de portada</h2>
+      <h2>Cabecera de la página de inicio</h2>
+      <p>Configura el título y texto por defecto en la cabecera de la página de inicio.</p>
+
       <table class="form-table">
         <tr valign="top">
           <th scope="row">Título del eslogan</th>
           <td><input type="text" name="home_hero_title" size="40" value="<?php echo get_option('home_hero_title'); ?>" />
-          <br><span class="description">Añade un titulo personalizado para el top de la página</span></td>
+          <br><span class="description">Añade un titulo personalizado para la cabecera de la página</span></td>
         </tr>
         <tr valign="top">
           <th scope="row">Texto del eslogan</th>
           <td><textarea name="home_hero_text" cols="37" rows="10"><?php echo get_option('home_hero_text'); ?></textarea>
-          <br><span class="description">Añade un texto personalizado para el top de la página</span></td>
+          <br><span class="description">Añade un texto personalizado para la cabecera de la página</span></td>
         </tr>
       </table>
 
       <hr>
 
-      
+      <!-- Seccion Destacada -->
+
+      <h2>Destacada</h2>
+      <p>Configura el contenido de la sección destacada bajo la cabecera.</p>
+
+      <table class="form-table">
+        <tr valign="top">
+          <th scope="row">Visibilidad</th>
+          <td>
+          <?php $options = get_option( "home_highlight_visibilidad" ); ?>
+          <input type="checkbox" name="home_highlight_visibilidad" <?php checked( $options, 1 ); ?> value="1"> <span class="description">Marcar para ocultar la sección</span>
+        </tr>
+      </table>
+
+      <table class="form-table">
+        <tr valign="top">
+          <th scope="row">Título de la sección destacada</th>
+          <td><input type="text" name="home_highlight_cabecera_title" size="40" value="<?php echo get_option('home_highlight_cabecera_title'); ?>" /></td>
+        </tr>
+        <tr valign="top">
+          <th scope="row">Texto de la sección destacada</th>
+          <td><textarea name="home_highlight_cabecera_text" cols="37" rows="10"><?php echo get_option('home_highlight_cabecera_text'); ?></textarea></td>
+        </tr>
+      </table>
+
+      <table class="form-table">
+        <tr valign="top">
+          <th scope="row">Imagen punto 1</th>
+          <td><input type="text" name="home_punto_uno_image" size="40" value="<?php echo get_option('home_punto_uno_image'); ?>" />
+          <br><span class="description">Inserta la URL para la imagen del punto 1</span></td>
+        </tr>
+        <tr valign="top">
+          <th scope="row">Texto del punto 1</th>
+          <td><textarea name="home_punto_uno_text" cols="37" rows="10"><?php echo get_option('home_punto_uno_text'); ?></textarea></td>
+        </tr>
+      </table>
+
+      <table class="form-table">
+        <tr valign="top">
+          <th scope="row">Imagen punto 2</th>
+          <td><input type="text" name="home_punto_dos_image" size="40" value="<?php echo get_option('home_punto_dos_image'); ?>" />
+          <br><span class="description">Inserta la URL para la imagen del punto 2</span></td>
+        </tr>
+        <tr valign="top">
+          <th scope="row">Texto del punto 2</th>
+          <td><textarea name="home_punto_dos_text" cols="37" rows="10"><?php echo get_option('home_punto_dos_text'); ?></textarea></td>
+        </tr>
+      </table>
+
+      <table class="form-table">
+        <tr valign="top">
+          <th scope="row">Imagen punto 3</th>
+          <td><input type="text" name="home_punto_tres_image" size="40" value="<?php echo get_option('home_punto_tres_image'); ?>" />
+          <br><span class="description">Inserta la URL para la imagen del punto 3</span></td>
+        </tr>
+        <tr valign="top">
+          <th scope="row">Texto del punto 3</th>
+          <td><textarea name="home_punto_tres_text" cols="37" rows="10"><?php echo get_option('home_punto_tres_text'); ?></textarea></td>
+        </tr>
+      </table>
+
+      <hr>
+
+      <!-- Seccion Features -->
+
+      <h2>Features</h2>
+      <p>Configura el contenido de la sección Features.</p>
+
+      <table class="form-table">
+        <tr valign="top">
+          <th scope="row">Visibilidad</th>
+          <td>
+          <?php $options = get_option( "home_features_visibilidad" ); ?>
+          <input type="checkbox" name="home_features_visibilidad" <?php checked( $options, 1 ); ?> value="1"> <span class="description">Marcar para ocultar la sección</span>
+        </tr>
+      </table>
+
+      <table class="form-table">
+        <tr valign="top">
+          <th scope="row">Título de la cabecera</th>
+          <td><input type="text" name="home_features_cabecera_title" size="40" value="<?php echo get_option('home_features_cabecera_title'); ?>" /></td>
+        </tr>
+        <tr valign="top">
+          <th scope="row">Texto de la cabecera</th>
+          <td><textarea name="home_features_cabecera_text" cols="37" rows="10"><?php echo get_option('home_features_cabecera_text'); ?></textarea></td>
+        </tr>
+        <tr valign="top">
+          <th scope="row">Imagen de la cabecera</th>
+          <td><input type="text" name="home_features_cabecera_image" size="40" value="<?php echo get_option('home_features_cabecera_image'); ?>" />
+          <br><span class="description">Inserta la URL para la imagen de la cabecera</span></td>
+        </tr>
+      </table>
+
+      <table class="form-table">
+        <tr valign="top">
+          <th scope="row">Título de Feature uno</th>
+          <td><input type="text" name="home_features_uno_title" size="40" value="<?php echo get_option('home_features_uno_title'); ?>" /></td>
+        </tr>
+        <tr valign="top">
+          <th scope="row">Texto de Feature uno</th>
+          <td><textarea name="home_features_uno_text" cols="37" rows="10"><?php echo get_option('home_features_uno_text'); ?></textarea></td>
+        </tr>
+      </table>
+
+      <table class="form-table">
+        <tr valign="top">
+          <th scope="row">Título de Feature dos</th>
+          <td><input type="text" name="home_features_dos_title" size="40" value="<?php echo get_option('home_features_dos_title'); ?>" /></td>
+        </tr>
+        <tr valign="top">
+          <th scope="row">Texto de Feature dos</th>
+          <td><textarea name="home_features_dos_text" cols="37" rows="10"><?php echo get_option('home_features_dos_text'); ?></textarea></td>
+        </tr>
+      </table>
+
+      <table class="form-table">
+        <tr valign="top">
+          <th scope="row">Título de Feature tres</th>
+          <td><input type="text" name="home_features_tres_title" size="40" value="<?php echo get_option('home_features_tres_title'); ?>" /></td>
+        </tr>
+        <tr valign="top">
+          <th scope="row">Texto de Feature tres</th>
+          <td><textarea name="home_features_tres_text" cols="37" rows="10"><?php echo get_option('home_features_tres_text'); ?></textarea></td>
+        </tr>
+      </table>
+
+      <hr>
 
       <p class="submit">
       	<input name="configuracion_guardar" type="submit" class="button-primary" value="<?php _e('Guardar cambios') ?>" />
