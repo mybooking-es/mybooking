1f6156b59c53bd032190e4fd45bb4872298cfb03
Marcel Reig <marcelreig@gmail.com>
2019-09-30 13:39:56 +0200

    working on flex-form

--- mybooking-templates/mybooking-plugin-selector-widget.php
+++ mybooking-templates/mybooking-plugin-selector-widget.php
@@ -3,8 +3,9 @@
   <!-- FLEXBOX -->
 
   <section class="widget widget_mybooking_rent_engine_selector reservation-step">
-    <div class="">
       <form name="widget_search_form" method="get" enctype="application/x-www-form-urlencoded" class="flex-form">
+      
+      <div class="box-1">
         <div class="flex-form-item-box">
           <label>Lugar Entrega </label>
           <div class="flex-form-item">
@@ -20,6 +21,8 @@
             <i class="fa fa-angle-down flex-icon"></i>
           </div>
         </div>
+      </div>
+      <div class="box-2">
         <div class="flex-form-item-box">
           <label>Fecha Entrega </label>
           <div class="flex-form-item">
@@ -38,9 +41,10 @@
             </select>
           </div>
         </div>
+      </div>
+     
         <div class="flex-form-item-box">
-          <input type="submit" class="btn btn-primary btn-block" value="Buscar" />
+          <input type="submit" class="btn btn-primary" value="Buscar" />
         </div>
       </form>
-    </div>
   </section>
