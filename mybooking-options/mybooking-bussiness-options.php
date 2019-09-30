af41da8b6ba6d3021c08b9c1de9fec29d0a824a4
Hector <hacheunomenorca@gmail.com>
2019-09-30 11:32:42 +0200

    Página de opciones del negocio

--- mybooking-options/mybooking-bussiness-options.php
+++ mybooking-options/mybooking-bussiness-options.php
@@ -1,38 +1,38 @@
 <?php
 /**
-*		CONFIGURACIÓN DE CONTACTO
-*  	-------------------------
+*		CONFIGURACIÓN DE LA EMPRESA
+*  	---------------------------
 * 	Autor: Hector Asencio @Mybooking
 * 	Versión: 0.0.1
 *  	@package Understrap Mybooking Child
 */
 
-add_action('admin_menu', 'mybookinges_crea_menu_contacto');
-add_action('admin_init', 'mybookinges_registra_opciones_contacto');
+add_action('admin_menu', 'mybookinges_crea_menu_info');
+add_action('admin_init', 'mybookinges_registra_opciones_info');
 
-function mybookinges_crea_menu_contacto() {
+function mybookinges_crea_menu_info() {
   if (!current_user_can('manage_options') || current_user_can('administrator'))
     add_submenu_page(
       "configuracion",
-    	__("Contacto"),
-    	__("Contacto"),
+    	__("Información del negocio"),
+    	__("Información del negocio"),
     	"edit_pages",
     	"contacto",
-    	"mybookinges_configuracion_contacto"
+    	"mybookinges_configuracion_info"
     	);
 }
 
-function mybookinges_registra_opciones_contacto() {
+function mybookinges_registra_opciones_info() {
 
   // Definición de opciones
   add_option("home_proyectos_titulo_seccion","","","yes");
 
   // Registro de opciones
-  register_setting("opciones_contacto", "home_proyectos_titulo_seccion");
+  register_setting("opciones_info", "home_proyectos_titulo_seccion");
 
 }
 
-function mybookinges_configuracion_contacto() {
+function mybookinges_configuracion_info() {
   if (!current_user_can('edit_pages'))
       wp_die(__("No tienes acceso a esta página."));
   ?>
@@ -42,7 +42,7 @@ function mybookinges_configuracion_contacto() {
     <!-- Titulo de la página -->
 
     <h1>
-      Configuración de la página de Contacto <br>
+      Configuración de datos de contacto del negocio<br>
       <small>Ajustes para el template 'MyBooking Contacto'</small>
     </h1>
 
@@ -54,7 +54,7 @@ function mybookinges_configuracion_contacto() {
 
     <form method="post" action="options.php">
 
-      <?php settings_fields('opciones_contacto'); ?>
+      <?php settings_fields('opciones_info'); ?>
 
 
 
