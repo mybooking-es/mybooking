3087a0c1afe71bd06f65ed8fa5112b59ca1c8d8b
Hector <hacheunomenorca@gmail.com>
2019-10-07 12:26:25 +0200

    Erased comments hook on pages and posts

---
+++ single.php
@@ -0,0 +1,53 @@
+<?php
+/**
+*		SINGLE POST
+*  	-----------
+*		Overrides parent document on Understrap Theme
+*
+* 	Autor: Hector Asencio @Mybooking
+* 	Versión: 0.0.1
+*  	@package Understrap Mybooking Child
+*/
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<div class="wrapper" id="single-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+
+			<!-- Do the left sidebar check -->
+			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+
+			<main class="site-main" id="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'loop-templates/content', 'single' ); ?>
+
+					<?php understrap_post_nav(); ?>
+
+					<!--
+					@Mybooking: We don't want comments on client's posts 
+					--------------------------------------------------------------------->
+
+				<?php endwhile; // end of the loop. ?>
+
+			</main><!-- #main -->
+
+			<!-- Do the right sidebar check -->
+			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div><!-- #single-wrapper -->
+
+<?php get_footer();
