a396e5e2d5347b234f5243b951cfd6a367108a5a
Marcel Reig <marcelreig@gmail.com>
2019-10-10 15:12:34 +0200

    edit extras css

--- mybooking-templates/mybooking-plugin-choose-product-tmpl.php
+++ mybooking-templates/mybooking-plugin-choose-product-tmpl.php
@@ -10,12 +10,22 @@
           <section class="footer-card">
               <div class="product-wrapper"><img src="<%=product.photo%>" class="product-image"></div>
               <p class="short-description"><%=product.short_description%></p>
-              <p class="product-price"><%=configuration.formatCurrency(product.price)%></p>
-              <% if (product.availability) { %>
-                <a class="btn btn-light mb30 btn-choose-product" data-product="<%=product.code%>">Seleccionar</a>
-              <% } else { %>
-                <p>Consulta telefónica</p>
+              <!-- Offer -->
+              <% if (product.price != product.base_price) { %>             
+                <% if (product.offer_discount_type == 'percentage') { %>
+                  <p class="lead mt-2"><%=new Number(product.offer_value)%>% <%=product.offer_name%></p>
+                <% } %>
+                <p><small class="text-muted"><s><%= configuration.formatCurrency(product.base_price)%></s></small></p>
               <% } %>
+              <% if (product.availability) { %>
+              <% if (product.few_available_units) { %>
+              <p class="text-danger">¡Quedan pocas unidades!</p>  
+              <% } %>              
+              <a class="btn btn-light mb30 btn-choose-product" data-product="<%=product.code%>">Seleccionar</a>
+              <% } else { %>
+              <p class="has-text-centered">Modelo no disponible en la oficina y fechas seleccionadas</p>
+              <% } %>
+              <p class="product-price"><%=configuration.formatCurrency(product.price)%></p>
           </section>
       </div>
       <% } %>
@@ -23,7 +33,7 @@
 
     <!-- Script detailed for reservation summary -->
     <script type="text/tmpl" id="script_reservation_summary">
-      <div class="bg-gray-100 p-3 mb-5 shadow-bottom">
+      <div class="bg-white p-3 mb-5 shadow-bottom">
         <h4 class="brand-primary my-3">Detalle de la reserva</h4>
         <h5>Entrega</h5>
         <p class="color-gray-700"><span><i class="fa fa-calendar mr-1" aria-hidden="true"></i></span><%=shopping_cart.date_from_full_format%> / <%=shopping_cart.time_from%></p>
--- mybooking-templates/mybooking-plugin-choose-product.php
+++ mybooking-templates/mybooking-plugin-choose-product.php
@@ -2,15 +2,17 @@
 
 <div class="reservation-step">
   <!-- Reservation : Pickup/Return information -->
-  <div class="container-fluid">
-    <div class="row my-5">
-      <div class="col-12 col-lg-3">
-        <div id="reservation_detail"></div>
-      </div>
-      <div class="col-12 col-lg-8">
-        <div id="product_listing" class="magic-cards mb-5"></div>
+  <div class="bg-gray-200 py-5">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12 col-lg-3">
+          <div id="reservation_detail"></div>
+        </div>
+        <div class="col-12 col-lg-8">
+          <div id="product_listing" class="magic-cards mb-5"></div>
+        </div>
       </div>
     </div>
+    <?php mybooking_engine_get_template('mybooking-plugin-modify-reservation.php') ?>
   </div>
-  <?php mybooking_engine_get_template('mybooking-plugin-modify-reservation.php') ?>
 </div>
