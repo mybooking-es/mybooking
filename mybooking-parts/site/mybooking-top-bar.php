eed90c6dbc67f005c988aaf5c2c28700c8c12063
Hector <hacheunomenorca@gmail.com>
2019-11-11 11:23:05 +0100

    Topbar refactoring & news align fix

--- mybooking-parts/site/mybooking-top-bar.php
+++ mybooking-parts/site/mybooking-top-bar.php
@@ -3,7 +3,7 @@
 *		SITE TOP BAR PARTIAL
 *  	--------------------
 *
-* 	Versión: 0.0.3
+* 	Versión: 0.0.4
 *   @package WordPress
 *   @subpackage Mybooking WordPress Theme
 *   @since Mybooking WordPress Theme 0.0.1
@@ -19,35 +19,26 @@ defined( 'ABSPATH' ) || exit;
 
     <?php $company_phone = get_option("company_info_phone");
         if ($company_phone !== '') { ?>
-          <i class="fa fa-phone" aria-hidden="true"></i>
-          <a href="tel:<?php echo $company_phone ?>"> <?php echo $company_phone ?></span> </a>
+          <a class="mybooking-topbar_info-link" href="tel:<?php echo $company_phone ?>"><i class="fa fa-phone" aria-hidden="true"></i> <span class="mybooking-topbar_info-link--text"><?php echo $company_phone ?></span></a>
     <?php } ?>
 
   </span>
-  <span class="ml-3">
+  <span>
 
     <?php $company_chat = get_option("company_info_chat");
         if ($company_chat !== '') { ?>
-          <i class="fa fa-whatsapp" aria-hidden="true"></i>
-          <a href="tel:<?php echo $company_chat ?>"> <?php echo $company_chat ?></span> </a>
+	  <a class="mybooking-topbar_info-link" href="tel:<?php echo $company_chat ?>"><i class="fa fa-whatsapp" aria-hidden="true"></i> <span class="mybooking-topbar_info-link--text"><?php echo $company_chat ?></span></a>
     <?php } ?>
 
   </span>
-  <span class="ml-3">
+  <span>
 
     <?php $company_email = get_option("company_info_email");
         if ($company_email !== '') { ?>
-          <i class="fa fa-envelope-o" aria-hidden="true"></i>
-          <a class="color-gray-800" href="mailto:<?php echo $company_email ?>"> <?php echo $company_email ?></span> </a>
+	  <a class="mybooking-topbar_info-link" href="mailto:<?php echo $company_email ?>"><i class="fa fa-envelope-o" aria-hidden="true"></i> <span class="mybooking-topbar_info-link--text"><?php echo $company_email ?></span></a>
     <?php } ?>
 
   </span>
-
-  <!-- Widget -->
-
-  <?php if ( is_active_sidebar( 'mybooking_top_bar' ) ) : ?>
-    <?php dynamic_sidebar( 'mybooking_top_bar' ); ?>
-  <?php endif; ?>
 
   <!-- Top Menu -->
 
