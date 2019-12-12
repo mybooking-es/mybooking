16bdd8626439940075205df200c11d12e1f0d11d
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2019-12-12 08:38:45 +0100

    custom address integration

--- mybooking-templates/mybooking-plugin-selector-widget.php
+++ mybooking-templates/mybooking-plugin-selector-widget.php
@@ -24,22 +24,44 @@
       <input type="hidden" name="family_id" value="<?php echo $args['family_id']?>"/>
 
     <?php endif; ?>
-
+    <!-- Pickup place -->
     <div class="flex-form-item-box">
       <label><?php _e('Lugar Entrega ', 'mybooking') ?></label>
-      <div class="flex-form-item">
+      <div class="flex-form-item widget_pickup_place_group">
         <select id="widget_pickup_place" name="pickup_place"></select>
         <i class="fa fa-angle-down flex-icon"></i>
       </div>
+      <!-- Custom delivery place -->
+      <div id="another_pickup_place_group" style="display: none;">
+        <div class="flex-form-item justify-content-between">
+          <input class="bg-white w-100" type="text" id="widget_pickup_place_other" name="pickup_place_other" />
+          <input type="hidden" name="custom_pickup_place" value="false" />
+          <button type="button" class="widget_another_pickup_place_group_close p-0">
+            <i class="fa fa-times flex-icon"></i>
+          </button>
+        </div>
+      </div>      
     </div>
+    <!-- Return place -->
     <div class="flex-form-item-box">
       <label><?php _e('Lugar Devolución ', 'mybooking') ?></label>
-      <div class="flex-form-item">
+      <div class="flex-form-item widget_return_place_group">
         <select id="widget_return_place" name="return_place">
         </select>
         <i class="fa fa-angle-down flex-icon"></i>
       </div>
+      <!-- Custom delivery place -->
+      <div id="another_return_place_group" style="display: none;">
+        <div class="flex-form-item bg-white justify-content-between">
+          <input class="bg-white w-100" type="text" id="widget_return_place_other" name="return_place_other" />
+          <input type="hidden" name="custom_return_place" value="false" />
+          <button type="button" class="widget_another_return_place_group_close p-0">
+            <i class="fa fa-times flex-icon"></i>
+          </button>
+        </div>
+      </div>      
     </div>
+    <!-- Date/Time From -->
     <div class="flex-form-item-box">
       <label><?php _e('Fecha Entrega ', 'mybooking') ?></label>
       <div class="flex-form-item">
@@ -49,6 +71,7 @@
         </select>
       </div>
     </div>
+    <!-- Date/Time To -->
     <div class="flex-form-item-box">
       <label><?php _e('Fecha Devolución ', 'mybooking') ?></label>
       <div class="flex-form-item">
