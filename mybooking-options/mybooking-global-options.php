71e46c50d7a82fb2453b96c0dbdda9feca497b5c
Hector <hacheunomenorca@gmail.com>
2020-01-23 15:48:57 +0100

    Footer fix

--- mybooking-options/mybooking-global-options.php
+++ mybooking-options/mybooking-global-options.php
@@ -103,8 +103,8 @@ function mybookinges_configuration_global() {
           <th scope="row"><?php _e('Footer', 'mybooking') ?></th>
           <td>
             <?php $options_footer = get_option( "global_footer_layout" ); ?>
-            <input type="radio" name="global_footer_layout" <?php checked( $options_footer, 0 ); ?> value="1"> <span class="description"><strong><?php _e('Normal', 'mybooking') ?></strong><br><?php _e('Muestra cuatro columnas para widgets, información corporativa y enlaces sociales', 'mybooking') ?></span>
-            <input type="radio" name="global_footer_layout" <?php checked( $options_footer, 1 ); ?> value="0"> <span class="description"><strong><?php _e('Mínimal','mybooking') ?></strong><br><?php _e('Solo muestra el copyright', 'mybooking') ?></span><br><br>
+            <input type="radio" name="global_footer_layout" <?php checked( $options_footer, 0 ); ?> value="0"> <span class="description"><strong><?php _e('Normal', 'mybooking') ?></strong><br><?php _e('Muestra cuatro columnas para widgets, información corporativa y enlaces sociales', 'mybooking') ?></span><br><br>
+            <input type="radio" name="global_footer_layout" <?php checked( $options_footer, 1 ); ?> value="1"> <span class="description"><strong><?php _e('Mínimal','mybooking') ?></strong><br><?php _e('Solo muestra el copyright', 'mybooking') ?></span>
           </td>
         </tr>
       </table>
