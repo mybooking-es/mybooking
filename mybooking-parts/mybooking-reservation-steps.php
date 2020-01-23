93922deb70b5615bc098cf88f04970f75c7cb3b6
Marcel Reig <marcelreig@gmail.com>
2020-01-23 14:07:16 +0100

    edit steps

--- mybooking-parts/mybooking-reservation-steps.php
+++ mybooking-parts/mybooking-reservation-steps.php
@@ -10,38 +10,27 @@
 */
 ?>
 
-<div class="steps_container">
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-lg-auto col-md-10">
-        <div id="steps" class="steps">
+<div class="steps-wrapper">
+  <div id="steps">
 
-          <?php
-        $classes = get_body_class();
+    <?php
+      $classes = get_body_class();
 
-        if ( in_array('choose_product',$classes) ) {
-          $choose_active = 'active';
-          $step = __('Seleccionar producto', 'mybooking');
-        } elseif ( in_array('complete',$classes) ) {
-          $complete_active = 'active';
-          $step = __('Completar reserva', 'mybooking');
-        } elseif ( in_array('summary',$classes) ) {
-          $summary_active = 'active';
-          $step = __('Resumen', 'mybooking');
-        } ?>
+      if ( in_array('choose_product',$classes) ) {
+        $choose_active = 'active';
+        $step = __('Seleccionar producto', 'mybooking');
+      } elseif ( in_array('complete',$classes) ) {
+        $complete_active = 'active';
+        $step = __('Completar reserva', 'mybooking');
+      } elseif ( in_array('summary',$classes) ) {
+        $summary_active = 'active';
+        $step = __('Resumen', 'mybooking');
+      } ?>
 
-          <div data-desc="<?php _e( 'Lugar y Fecha', 'mybooking' ) ?>" class="step">1</div>
-          <div data-desc="<?php _e( 'Seleccionar producto', 'mybooking' ) ?>"
-            class="<?php echo $choose_active ?> step">2</div>
-          <div data-desc="<?php _e( 'Completar Reserva', 'mybooking' ) ?>"
-            class="<?php echo $complete_active ?> step">3</div>
-          <div data-desc="<?php _e( 'Sumario', 'mybooking' ) ?>"
-            class="<?php echo $summary_active ?> step">4</div>
-        </div>
-      </div>
-      <div class="col-md-12 col-lg-4 d-flex">
-        <h2 class="steps_title"><?php echo _e( $step, 'mybooking') ?></h2>
-      </div>
-    </div>
+    <div data-desc="<?php _e( 'Lugar y Fecha', 'mybooking' ) ?>" class="step">1</div>
+    <div data-desc="<?php _e( 'Seleccionar producto', 'mybooking' ) ?>" class="<?php echo $choose_active ?> step">2</div>
+    <div data-desc="<?php _e( 'Completar Reserva', 'mybooking' ) ?>" class="<?php echo $complete_active ?> step">3</div>
+    <div data-desc="<?php _e( 'Sumario', 'mybooking' ) ?>" class="<?php echo $summary_active ?> step">4</div>
   </div>
+  <h2 class="steps_title"><?php echo _e( $step, 'mybooking') ?></h2>
 </div>
