8d9bf34dff0d1deb993ecedf694f178d40d3a94d
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2020-02-17 12:41:42 +0100

    removed child theme js

--- functions.php
+++ functions.php
@@ -25,22 +25,6 @@ function mybooking_setup(){
 }
 add_action( 'after_setup_theme', 'mybooking_setup' );
 
-
-/**
- * Clean inherited CSS & scripts
- *
- */
-/*
-function understrap_remove_scripts() {
-    wp_dequeue_style( 'understrap-styles' );
-    wp_deregister_style( 'understrap-styles' );
-    wp_dequeue_script( 'understrap-scripts' );
-    wp_deregister_script( 'understrap-scripts' );
-    // Removes the parent themes stylesheet and scripts from inc/enqueue.php
-}
-add_action( 'wp_enqueue_scripts', 'understrap_remove_scripts', 20 );
-*/
-
 /**
  * Enqueue new CSS & scripts
  *
@@ -50,27 +34,11 @@ function theme_enqueue_styles() {
 	// Get the theme data
 	$the_theme = wp_get_theme();
 
-    // == Load CSS
-    // Load parent theme CSS
-//    wp_enqueue_style( 'mybooking-styles', get_template_directory_uri() . '/css/theme.min.css', array(), $the_theme->get( 'Version' ) );
-    wp_enqueue_style( 'mybooking-styles', get_stylesheet_directory_uri() .  '/css/theme.min.css', array(), $the_theme->get( 'Version' ) );
+  // == Load CSS
+  wp_enqueue_style( 'mybooking-styles', get_stylesheet_directory_uri() .  '/css/theme.min.css', array(), $the_theme->get( 'Version' ) );
 
-    // Load child theme CSS
-    /*
-    wp_enqueue_style( 'child-understrap-mybooking-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
-    */
-    // == Load JS
-    // Load parent theme JS
-    
-//    wp_enqueue_script( 'mybooking-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $the_theme->get( 'Version' ), true );
-    wp_enqueue_script( 'mybooking-scripts', get_stylesheet_directory_uri() . '/js/theme.min.js', array(), $the_theme->get( 'Version' ), true );
-
-    /*
-    // Load child theme JS
-    wp_enqueue_script( 'child-understrap-mybooking-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
-    */
-    // Load child theme JS
-    //wp_enqueue_script( 'child-understrap-mybooking-scripts', get_stylesheet_directory_uri() . '/js/vendor/jquery-eu-cookie-law-popup.js', array(), $the_theme->get( 'Version' ), true );
+  // == Load JS
+  wp_enqueue_script( 'mybooking-scripts', get_stylesheet_directory_uri() . '/js/theme.min.js', array(), $the_theme->get( 'Version' ), true );
 
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
