c8dff1d6ec11bab48c1589a93b47fc04d8b89e79
Hector <hacheunomenorca@gmail.com>
2020-02-20 09:56:55 +0100

    Solves Elementor container desplacement

--- mybooking-parts/home/mybooking-home-content.php
+++ mybooking-parts/home/mybooking-home-content.php
@@ -15,10 +15,12 @@ defined( 'ABSPATH' ) || exit;
 
 <div class="container page_content">
   <div class="row">
+    <div class="col">
 
-    <?php while ( have_posts() ) : the_post(); ?>
-      <?php the_content(); ?>
-    <?php endwhile;?>
-
+      <?php while ( have_posts() ) : the_post(); ?>
+        <?php the_content(); ?>
+      <?php endwhile;?>
+      
+    </div>
   </div>
 </div>
