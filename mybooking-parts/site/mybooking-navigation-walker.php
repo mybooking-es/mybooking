878412f782e716eb6f5d82e31bf4a9c92f9c98c1
Hector <hacheunomenorca@gmail.com>
2020-03-27 22:23:19 +0100

    Solves issue #11

--- mybooking-parts/site/mybooking-navigation-walker.php
+++ mybooking-parts/site/mybooking-navigation-walker.php
@@ -21,7 +21,7 @@ defined( 'ABSPATH' ) || exit;
     'menu_class'      => 'navbar-nav ml-auto',
     'fallback_cb'     => '',
     'menu_id'         => 'main-menu',
-    'depth'           => 4,
+    'depth'           => 2,
     'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
   )
 ); ?>
