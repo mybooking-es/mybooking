b4e13c4f685cc66d73ec2c93cdb9f00258632825
Marcel Reig <marcelreig@gmail.com>
2020-05-15 18:51:28 +0200

    edit cookies scss

--- js/theme.js
+++ js/theme.js
@@ -9578,243 +9578,254 @@ if (typeof Object.create !== "function") {
  *
  */
 
-(function($) {
+(function ($) {
+  // for ie9 doesn't support debug console >>>
+  if (!window.console) window.console = {};
+  if (!window.console.log) window.console.log = function () {};
+  // ^^^
 
-// for ie9 doesn't support debug console >>>
-if (!window.console) window.console = {};
-if (!window.console.log) window.console.log = function () { };
-// ^^^
+  $.fn.euCookieLawPopup = function () {
+    var _self = this;
 
-$.fn.euCookieLawPopup = (function() {
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // PARAMETERS (MODIFY THIS PART) //////////////////////////////////////////////////////////////
+    _self.params = {
+      cookiePolicyUrl: "/cookies",
+      popupPosition: "top",
+      colorStyle: "default",
+      compactStyle: true,
+      popupTitle: "This website is using cookies",
+      popupText:
+        "We use cookies to ensure that we give you the best experience on our website. If you continue without changing your settings, we'll assume that you are happy to receive all cookies on this website.",
+      buttonContinueTitle: "Continue",
+      buttonLearnmoreTitle: "Learn&nbsp;more",
+      buttonLearnmoreOpenInNewWindow: true,
+      agreementExpiresInDays: 30,
+      autoAcceptCookiePolicy: false,
+      htmlMarkup: null,
+    };
 
-	var _self = this;
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // VARIABLES USED BY THE FUNCTION (DON'T MODIFY THIS PART) ////////////////////////////////////
+    _self.vars = {
+      INITIALISED: false,
+      HTML_MARKUP: null,
+      COOKIE_NAME: "EU_COOKIE_LAW_CONSENT",
+    };
 
-	///////////////////////////////////////////////////////////////////////////////////////////////
-	// PARAMETERS (MODIFY THIS PART) //////////////////////////////////////////////////////////////
-	_self.params = {
-		cookiePolicyUrl : '/cookies',
-		popupPosition : 'top',
-		colorStyle : 'default',
-		compactStyle : true,
-		popupTitle : 'This website is using cookies',
-		popupText : 'We use cookies to ensure that we give you the best experience on our website. If you continue without changing your settings, we\'ll assume that you are happy to receive all cookies on this website.',
-		buttonContinueTitle : 'Continue',
-		buttonLearnmoreTitle : 'Learn&nbsp;more',
-		buttonLearnmoreOpenInNewWindow : true,
-		agreementExpiresInDays : 30,
-		autoAcceptCookiePolicy : false,
-		htmlMarkup : null
-	};
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // PRIVATE FUNCTIONS FOR MANIPULATING DATA ////////////////////////////////////////////////////
 
-	///////////////////////////////////////////////////////////////////////////////////////////////
-	// VARIABLES USED BY THE FUNCTION (DON'T MODIFY THIS PART) ////////////////////////////////////
-	_self.vars = {
-		INITIALISED : false,
-		HTML_MARKUP : null,
-		COOKIE_NAME : 'EU_COOKIE_LAW_CONSENT'
-	};
+    // Overwrite default parameters if any of those is present
+    var parseParameters = function (object, markup, settings) {
+      if (object) {
+        var className = $(object).attr("class") ? $(object).attr("class") : "";
+        if (className.indexOf("eupopup-top") > -1) {
+          _self.params.popupPosition = "top";
+        } else if (className.indexOf("eupopup-fixedtop") > -1) {
+          _self.params.popupPosition = "fixedtop";
+        } else if (className.indexOf("eupopup-bottomright") > -1) {
+          _self.params.popupPosition = "bottomright";
+        } else if (className.indexOf("eupopup-bottomleft") > -1) {
+          _self.params.popupPosition = "bottomleft";
+        } else if (className.indexOf("eupopup-bottom") > -1) {
+          _self.params.popupPosition = "bottom";
+        } else if (className.indexOf("eupopup-block") > -1) {
+          _self.params.popupPosition = "block";
+        }
+        if (className.indexOf("eupopup-color-default") > -1) {
+          _self.params.colorStyle = "default";
+        } else if (className.indexOf("eupopup-color-inverse") > -1) {
+          _self.params.colorStyle = "inverse";
+        }
+        if (className.indexOf("eupopup-style-compact") > -1) {
+          _self.params.compactStyle = true;
+        }
+      }
 
-	///////////////////////////////////////////////////////////////////////////////////////////////
-	// PRIVATE FUNCTIONS FOR MANIPULATING DATA ////////////////////////////////////////////////////
+      if (markup) {
+        _self.params.htmlMarkup = markup;
+      }
 
-	// Overwrite default parameters if any of those is present
-	var parseParameters = function(object, markup, settings) {
+      if (settings) {
+        if (typeof settings.cookiePolicyUrl !== "undefined") {
+          _self.params.cookiePolicyUrl = settings.cookiePolicyUrl;
+        }
+        if (typeof settings.popupPosition !== "undefined") {
+          _self.params.popupPosition = settings.popupPosition;
+        }
+        if (typeof settings.colorStyle !== "undefined") {
+          _self.params.colorStyle = settings.colorStyle;
+        }
+        if (typeof settings.popupTitle !== "undefined") {
+          _self.params.popupTitle = settings.popupTitle;
+        }
+        if (typeof settings.popupText !== "undefined") {
+          _self.params.popupText = settings.popupText;
+        }
+        if (typeof settings.buttonContinueTitle !== "undefined") {
+          _self.params.buttonContinueTitle = settings.buttonContinueTitle;
+        }
+        if (typeof settings.buttonLearnmoreTitle !== "undefined") {
+          _self.params.buttonLearnmoreTitle = settings.buttonLearnmoreTitle;
+        }
+        if (typeof settings.buttonLearnmoreOpenInNewWindow !== "undefined") {
+          _self.params.buttonLearnmoreOpenInNewWindow =
+            settings.buttonLearnmoreOpenInNewWindow;
+        }
+        if (typeof settings.agreementExpiresInDays !== "undefined") {
+          _self.params.agreementExpiresInDays = settings.agreementExpiresInDays;
+        }
+        if (typeof settings.autoAcceptCookiePolicy !== "undefined") {
+          _self.params.autoAcceptCookiePolicy = settings.autoAcceptCookiePolicy;
+        }
+        if (typeof settings.htmlMarkup !== "undefined") {
+          _self.params.htmlMarkup = settings.htmlMarkup;
+        }
+      }
+    };
 
-		if (object) {
-			var className = $(object).attr('class') ? $(object).attr('class') : '';
-			if (className.indexOf('eupopup-top') > -1) {
-				_self.params.popupPosition = 'top';
-			}
-			else if (className.indexOf('eupopup-fixedtop') > -1) {
-				_self.params.popupPosition = 'fixedtop';
-			}
-			else if (className.indexOf('eupopup-bottomright') > -1) {
-				_self.params.popupPosition = 'bottomright';
-			}
-			else if (className.indexOf('eupopup-bottomleft') > -1) {
-				_self.params.popupPosition = 'bottomleft';
-			}
-			else if (className.indexOf('eupopup-bottom') > -1) {
-				_self.params.popupPosition = 'bottom';
-			}
-			else if (className.indexOf('eupopup-block') > -1) {
-				_self.params.popupPosition = 'block';
-			}
-			if (className.indexOf('eupopup-color-default') > -1) {
-				_self.params.colorStyle = 'default';
-			}
-			else if (className.indexOf('eupopup-color-inverse') > -1) {
-				_self.params.colorStyle = 'inverse';
-			}
-			if (className.indexOf('eupopup-style-compact') > -1) {
-				_self.params.compactStyle = true;
-			}
-		}
+    var createHtmlMarkup = function () {
+      if (_self.params.htmlMarkup) {
+        return _self.params.htmlMarkup;
+      }
 
-		if (markup) {
-			_self.params.htmlMarkup = markup;
-		}
+      var html =
+        '<div class="eupopup-container' +
+        " eupopup-container-" +
+        _self.params.popupPosition +
+        (_self.params.compactStyle ? " eupopup-style-compact" : "") +
+        " eupopup-color-" +
+        _self.params.colorStyle +
+        '">' +
+        '<div class="eupopup-head">' +
+        _self.params.popupTitle +
+        "</div>" +
+        '<div class="eupopup-body">' +
+        _self.params.popupText +
+        "</div>" +
+        '<div class="eupopup-buttons">' +
+        '<a href="#" class="eupopup-button eupopup-button_1">' +
+        _self.params.buttonContinueTitle +
+        "</a>" +
+        '<a href="' +
+        _self.params.cookiePolicyUrl +
+        '"' +
+        (_self.params.buttonLearnmoreOpenInNewWindow ? " target=_blank " : "") +
+        ' class="eupopup-button eupopup-button_2">' +
+        _self.params.buttonLearnmoreTitle +
+        "</a>" +
+        '<div class="clearfix"></div>' +
+        "</div>" +
+        '<a href="#" class="eupopup-closebutton"><i class="fas fa-times"></i></a>' +
+        "</div>";
 
-		if (settings) {
-			if (typeof settings.cookiePolicyUrl !== 'undefined') {
-				_self.params.cookiePolicyUrl = settings.cookiePolicyUrl;
-			}
-			if (typeof settings.popupPosition !== 'undefined') {
-				_self.params.popupPosition = settings.popupPosition;
-			}
-			if (typeof settings.colorStyle !== 'undefined') {
-				_self.params.colorStyle = settings.colorStyle;
-			}
-			if (typeof settings.popupTitle !== 'undefined') {
-				_self.params.popupTitle = settings.popupTitle;
-			}
-			if (typeof settings.popupText !== 'undefined') {
-				_self.params.popupText = settings.popupText;
-			}
-			if (typeof settings.buttonContinueTitle !== 'undefined') {
-				_self.params.buttonContinueTitle = settings.buttonContinueTitle;
-			}
-			if (typeof settings.buttonLearnmoreTitle !== 'undefined') {
-				_self.params.buttonLearnmoreTitle = settings.buttonLearnmoreTitle;
-			}
-			if (typeof settings.buttonLearnmoreOpenInNewWindow !== 'undefined') {
-				_self.params.buttonLearnmoreOpenInNewWindow = settings.buttonLearnmoreOpenInNewWindow;
-			}
-			if (typeof settings.agreementExpiresInDays !== 'undefined') {
-				_self.params.agreementExpiresInDays = settings.agreementExpiresInDays;
-			}
-			if (typeof settings.autoAcceptCookiePolicy !== 'undefined') {
-				_self.params.autoAcceptCookiePolicy = settings.autoAcceptCookiePolicy;
-			}
-			if (typeof settings.htmlMarkup !== 'undefined') {
-				_self.params.htmlMarkup = settings.htmlMarkup;
-			}
-		}
+      return html;
+    };
 
-	};
+    // Storing the consent in a cookie
+    var setUserAcceptsCookies = function (consent) {
+      var d = new Date();
+      var expiresInDays =
+        _self.params.agreementExpiresInDays * 24 * 60 * 60 * 1000;
+      d.setTime(d.getTime() + expiresInDays);
+      var expires = "expires=" + d.toGMTString();
+      document.cookie =
+        _self.vars.COOKIE_NAME + "=" + consent + "; " + expires + ";path=/";
 
-	var createHtmlMarkup = function() {
+      $(document).trigger("user_cookie_consent_changed", { consent: consent });
+    };
 
-		if (_self.params.htmlMarkup) {
-			return _self.params.htmlMarkup;
-		}
+    // Let's see if we have a consent cookie already
+    var userAlreadyAcceptedCookies = function () {
+      var userAcceptedCookies = false;
+      var cookies = document.cookie.split(";");
+      for (var i = 0; i < cookies.length; i++) {
+        var c = cookies[i].trim();
+        if (c.indexOf(_self.vars.COOKIE_NAME) !== -1) {
+          userAcceptedCookies = c.substring(
+            _self.vars.COOKIE_NAME.length + 1,
+            c.length
+          );
+        }
+      }
 
-		var html =
-			'<div class="eupopup-container' +
-			    ' eupopup-container-' + _self.params.popupPosition +
-			    (_self.params.compactStyle ? ' eupopup-style-compact' : '') +
-				' eupopup-color-' + _self.params.colorStyle + '">' +
-				'<div class="eupopup-head">' + _self.params.popupTitle + '</div>' +
-				'<div class="eupopup-body">' + _self.params.popupText + '</div>' +
-				'<div class="eupopup-buttons">' +
-				  '<a href="#" class="eupopup-button eupopup-button_1">' + _self.params.buttonContinueTitle + '</a>' +
-				  '<a href="' + _self.params.cookiePolicyUrl + '"' +
-				 	(_self.params.buttonLearnmoreOpenInNewWindow ? ' target=_blank ' : '') +
-					' class="eupopup-button eupopup-button_2">' + _self.params.buttonLearnmoreTitle + '</a>' +
-				  '<div class="clearfix"></div>' +
-				'</div>' +
-				'<a href="#" class="eupopup-closebutton">x</a>' +
-			'</div>';
+      return userAcceptedCookies;
+    };
 
-		return html;
-	};
+    var hideContainer = function () {
+      // $('.eupopup-container').slideUp(200);
+      $(".eupopup-container").animate(
+        {
+          opacity: 0,
+          height: 0,
+        },
+        200,
+        function () {
+          $(".eupopup-container").hide(0);
+        }
+      );
+    };
 
-	// Storing the consent in a cookie
-	var setUserAcceptsCookies = function(consent) {
-		var d = new Date();
-		var expiresInDays = _self.params.agreementExpiresInDays * 24 * 60 * 60 * 1000;
-		d.setTime( d.getTime() + expiresInDays );
-		var expires = "expires=" + d.toGMTString();
-		document.cookie = _self.vars.COOKIE_NAME + '=' + consent + "; " + expires + ";path=/";
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // PUBLIC FUNCTIONS  //////////////////////////////////////////////////////////////////////////
+    var publicfunc = {
+      // INITIALIZE EU COOKIE LAW POPUP /////////////////////////////////////////////////////////
+      init: function (settings) {
+        parseParameters(
+          $(".eupopup").first(),
+          $(".eupopup-markup").html(),
+          settings
+        );
 
-		$(document).trigger("user_cookie_consent_changed", {'consent' : consent});
-	};
+        // No need to display this if user already accepted the policy
+        if (userAlreadyAcceptedCookies()) {
+          $(document).trigger("user_cookie_already_accepted", {
+            consent: true,
+          });
+          return;
+        }
 
-	// Let's see if we have a consent cookie already
-	var userAlreadyAcceptedCookies = function() {
-		var userAcceptedCookies = false;
-		var cookies = document.cookie.split(";");
-		for (var i = 0; i < cookies.length; i++) {
-			var c = cookies[i].trim();
-			if (c.indexOf(_self.vars.COOKIE_NAME) !== -1) {
-				userAcceptedCookies = c.substring(_self.vars.COOKIE_NAME.length + 1, c.length);
-			}
-		}
+        // We should initialise only once
+        if (_self.vars.INITIALISED) {
+          return;
+        }
+        _self.vars.INITIALISED = true;
 
-		return userAcceptedCookies;
-	};
+        // Markup and event listeners >>>
+        _self.vars.HTML_MARKUP = createHtmlMarkup();
 
-	var hideContainer = function() {
-		// $('.eupopup-container').slideUp(200);
-		$('.eupopup-container').animate({
-			opacity: 0,
-			height: 0
-		}, 200, function() {
-			$('.eupopup-container').hide(0);
-		});
-	};
+        if ($(".eupopup-block").length > 0) {
+          $(".eupopup-block").append(_self.vars.HTML_MARKUP);
+        } else {
+          $("BODY").append(_self.vars.HTML_MARKUP);
+        }
 
-	///////////////////////////////////////////////////////////////////////////////////////////////
-	// PUBLIC FUNCTIONS  //////////////////////////////////////////////////////////////////////////
-	var publicfunc = {
+        $(".eupopup-button_1").click(function () {
+          setUserAcceptsCookies(true);
+          hideContainer();
+          return false;
+        });
+        $(".eupopup-closebutton").click(function () {
+          setUserAcceptsCookies(true);
+          hideContainer();
+          return false;
+        });
+        // ^^^ Markup and event listeners
 
-		// INITIALIZE EU COOKIE LAW POPUP /////////////////////////////////////////////////////////
-		init : function(settings) {
+        // Ready to start!
+        $(".eupopup-container").show();
 
-			parseParameters(
-				$(".eupopup").first(),
-				$(".eupopup-markup").html(),
-				settings);
+        // In case it's alright to just display the message once
+        if (_self.params.autoAcceptCookiePolicy) {
+          setUserAcceptsCookies(true);
+        }
+      },
+    };
 
-			// No need to display this if user already accepted the policy
-			if (userAlreadyAcceptedCookies()) {
-        $(document).trigger("user_cookie_already_accepted", {'consent': true});
-				return;
-			}
-
-			// We should initialise only once
-			if (_self.vars.INITIALISED) {
-				return;
-			}
-			_self.vars.INITIALISED = true;
-
-			// Markup and event listeners >>>
-			_self.vars.HTML_MARKUP = createHtmlMarkup();
-
-			if ($('.eupopup-block').length > 0) {
-				$('.eupopup-block').append(_self.vars.HTML_MARKUP);
-			} else {
-				$('BODY').append(_self.vars.HTML_MARKUP);
-			}
-
-			$('.eupopup-button_1').click(function() {
-				setUserAcceptsCookies(true);
-				hideContainer();
-				return false;
-			});
-			$('.eupopup-closebutton').click(function() {
-				setUserAcceptsCookies(true);
-				hideContainer();
-				return false;
-			});
-			// ^^^ Markup and event listeners
-
-			// Ready to start!
-			$('.eupopup-container').show();
-
-			// In case it's alright to just display the message once
-			if (_self.params.autoAcceptCookiePolicy) {
-				setUserAcceptsCookies(true);
-			}
-
-		}
-
-	};
-
-	return publicfunc;
-});
-
-}(jQuery));
+    return publicfunc;
+  };
+})(jQuery);
 
 /**
  *   MYBOOKING CUSTOM JS
@@ -9855,14 +9866,7 @@ $(document).ready(function () {
 $(document).ready(function () {
   if (typeof $(".home-header_sticky-breakpoint").offset() !== "undefined") {
     var height = $(".home-header_sticky-breakpoint").offset().top;
-
     var md = new MobileDetect(window.navigator.userAgent);
-/*
-    var is_mobile = false;
-    if ($(".navbar-toggler").is(":visible")) {
-      is_mobile = true;
-    }
-*/
     if (!md.mobile()) {
       $(window).on("scroll", function () {
         if ($(".navbar-toggler").is(":visible")) {
@@ -9875,13 +9879,11 @@ $(document).ready(function () {
           $("#home-header_content_container").addClass(
             "home-header_content_container_sticky"
           );
-          // Elementor compatibility
         } else {
           $("#form-selector").removeClass("flex-form-sticky");
           $("#home-header_content_container").removeClass(
             "home-header_content_container_sticky"
           );
-          // Elementor compatibility
         }
       });
     }
