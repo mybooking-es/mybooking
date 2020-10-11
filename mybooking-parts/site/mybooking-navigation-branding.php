5c6d1752f4c416317621f163089444abab001e23
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2020-10-11 18:01:09 +0200

    brand logo + custom logo

--- mybooking-parts/site/mybooking-navigation-branding.php
+++ mybooking-parts/site/mybooking-navigation-branding.php
@@ -12,17 +12,32 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-// NOTE: This line is single client legacy code, we need to refactorize in orden to
-// avoid debug warnings messages
-$custom_logo = get_post_meta($post->ID, 'logo_personalizado', true)
 ?>
 
-<?php if ( ! has_custom_logo() ) { ?>
+<?php if ( has_custom_logo() ) { 
+    // Custom logo
+    // Check if the post has a custom logo
+    global $post;
+    if ( isset($post) ) {
+      $custom_logo = get_post_meta($post->ID, 'custom_logo', true);
+      if ( !empty( $custom_logo ) ) { ?>
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand custom-logo-link" rel="home">
+          <img src="<?php echo $custom_logo; ?>" class="img-fluid">
+        </a>
+<?php } 
+      else {
+        the_custom_logo();
+      } 
+    } 
+    else {
+      the_custom_logo();
+    }
+} else { 
 
-  <?php if ( is_front_page() && is_home() ) : ?>
+  // Not custom logo
+  if ( is_front_page() && is_home() ) { ?>
 
     <!-- Puts sitename and description inside H1 tag only at home -->
-
     <h1 class="navbar-brand mb-0">
       <a
         rel="home"
@@ -33,10 +48,9 @@ $custom_logo = get_post_meta($post->ID, 'logo_personalizado', true)
       </a>
     </h1>
 
-  <?php else : ?>
+  <?php } else { ?>
 
     <!-- Puts sitename and description everywhere but home -->
-
     <a
       class="navbar-brand"
       rel="home"
@@ -46,19 +60,6 @@ $custom_logo = get_post_meta($post->ID, 'logo_personalizado', true)
         <?php bloginfo( 'name' ); ?>
     </a>
 
-  <?php endif; ?>
-
-<?php
-// NOTE: This block is single client legacy code, we need to refactorize in orden to
-// avoid debug warnings messages
-} elseif ( !empty( $custom_logo ) ) { ?>
-
-    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand custom-logo-link" rel="home">
-      <img src="<?php echo $custom_logo; ?>" class="img-fluid">
-    </a>
-
-<?php } else { ?>
-
-  <?php the_custom_logo(); ?>
+  <?php } ?>
 
 <?php } ?>
