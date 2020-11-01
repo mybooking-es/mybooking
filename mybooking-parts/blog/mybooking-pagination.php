df1a117beb6600988d553cffa29505050d4187ed
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2020-11-01 20:58:34 +0100

    pagination

--- mybooking-parts/blog/mybooking-pagination.php
+++ mybooking-parts/blog/mybooking-pagination.php
@@ -12,33 +12,35 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-$prev_text = '<span aria-hidden="true">&laquo;</span>';
-$next_text = '<span aria-hidden="true">&raquo;</span>';
+$mybooking_prev_text = sprintf('<span aria-hidden="true">%s</span>',
+	                             esc_html_x( '&laquo;', 'pagination', 'mybooking' ) );
+$mybooking_next_text = sprintf('<span aria-hidden="true">%s</span>',
+	                             esc_html_x( '&raquo;', 'pagination', 'mybooking' ) );
 
-$posts_pagination = get_the_posts_pagination(
+$mybooking_posts_pagination = get_the_posts_pagination(
 	array(
+		'screen_reader_text' => _x( 'Posts navigation', 'pagination', 'mybooking' ),
 		'mid_size'  => 2,
-    'prev_next' => true,
-		'prev_text' => $prev_text,
-		'next_text' => $next_text,
+		'prev_text' => $mybooking_prev_text,
+		'next_text' => $mybooking_next_text,
 	)
 );
 
 // If we're not outputting the previous page link, prepend a placeholder with `visibility: hidden` to take its place.
-if ( strpos( $posts_pagination, 'prev page-numbers' ) === false ) {
-	$posts_pagination = str_replace( '<div class="nav-links">', '<div class="nav-links"><span class="prev page-numbers placeholder" aria-hidden="true">' . $prev_text . '</span>', $posts_pagination );
+if ( strpos( $mybooking_posts_pagination, 'prev page-numbers' ) === false ) {
+	$mybooking_posts_pagination = str_replace( '<div class="nav-links">', '<div class="nav-links"><span class="prev page-numbers placeholder" aria-hidden="true">' . $mybooking_prev_text . '</span>', $mybooking_posts_pagination );
 }
 
 // If we're not outputting the next page link, append a placeholder with `visibility: hidden` to take its place.
-if ( strpos( $posts_pagination, 'next page-numbers' ) === false ) {
-	$posts_pagination = str_replace( '</div>', '<span class="next page-numbers placeholder" aria-hidden="true">' . $next_text . '</span></div>', $posts_pagination );
+if ( strpos( $mybooking_posts_pagination, 'next page-numbers' ) === false ) {
+	$mybooking_posts_pagination = str_replace( '</div>', '<span class="next page-numbers placeholder" aria-hidden="true">' . $mybooking_next_text . '</span></div>', $mybooking_posts_pagination );
 }
 
-if ( $posts_pagination ) { ?>
+if ( $mybooking_posts_pagination ) { ?>
 
 	<div class="pagination-wrapper section-inner">
 
-		<?php echo $posts_pagination; ?>
+		<?php echo $mybooking_posts_pagination; //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- already escaped during generation. ?>
 
 	</div><!-- .pagination-wrapper -->
 
