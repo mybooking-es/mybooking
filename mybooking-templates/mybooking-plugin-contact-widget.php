c1f317f88e06578f0a6837a6b118d74f38d5ffa5
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2021-02-20 16:49:50 +0100

    new fields and recaptcha validation on contact form

--- mybooking-templates/mybooking-plugin-contact-widget.php
+++ mybooking-templates/mybooking-plugin-contact-widget.php
@@ -9,6 +9,22 @@
 <section class="widget widget_mybooking_engine_contact">
   <form id="widget_contact_form" name="widget_contact_form" autocomplete="off">
 
+          <?php if ( array_key_exists('subject', $args) && $args['subject'] != '' ) : ?>
+            <input type="hidden" name="subject" value="<?php echo esc_attr( $args['subject'] )?>" />
+          <?php endif; ?>
+
+          <?php if ( array_key_exists('source', $args) && $args['source'] != '' ) : ?>
+            <input type="hidden" name="source" value="<?php echo esc_attr( $args['source'] )?>" />
+          <?php endif; ?>
+
+          <?php if ( array_key_exists('sales_channel_code', $args) && $args['sales_channel_code'] != '' ) : ?>
+            <input type="hidden" name="sales_channel_code" value="<?php echo esc_attr( $args['sales_channel_code'] )?>" />
+          <?php endif; ?>
+
+          <?php if ( array_key_exists('rental_location_code', $args) && $args['rental_location_code'] != '' ) : ?>
+            <input type="hidden" name="rental_location_code" value="<?php echo esc_attr( $args['rental_location_code'] )?>" />
+          <?php endif; ?>
+
           <div class="field field-body">
             <div class="field">
               <label class="label"><?php echo esc_html_x( 'Name', 'contact_form', 'mybooking') ?>*</label>
@@ -50,6 +66,10 @@
               <div class="message is-danger" id="contact_form_errors" style="display: none">
                </div>
           </div>
+
+          <?php if ( array_key_exists('google_captcha_api_key', $args) && !empty( $args['google_captcha_api_key'] ) : ?>
+          <div class="g-recaptcha mt-1 mb-3" data-sitekey="<?php echo esc_attr( $args['google_captcha_api_key'] )?>"></div>
+          <?php endif; ?>
 
           <div class="field is-grouped">
             <div class="control">
