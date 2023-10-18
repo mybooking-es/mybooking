0199dff5c375e48bac7b5fab7041c26d52decb87
Juan Miqueo <jgil@MacBook-Air-de-Juan.local>
2023-10-18 11:20:21 +0200

    action hooks to modify plugin

--- inc/mybooking-engine-hooks.php
+++ inc/mybooking-engine-hooks.php
@@ -15,3 +15,17 @@ if ( !function_exists( 'mybooking_plugin_reservation_process_header_callback' ) 
   }
 }
 add_action( 'mybooking_plugin_reservation_process_header', 'mybooking_plugin_reservation_process_header_callback');
+
+// Append the sticky
+if ( !function_exists( 'mybooking_plugin_reservation_summary_callback' ) ) {
+  function mybooking_plugin_reservation_summary_callback() {
+    $template_path = 'mybooking-templates/';
+    $template_name = 'mybooking-reservation-sticky-tmpl.php';
+		$template_file = locate_template( array($template_path . $template_name, $template_name ) );
+    if ( file_exists( $template_file ) ) :
+      include $template_file;
+    endif;
+  }
+}
+add_action( 'mybooking_plugin_reservation_summary', 'mybooking_plugin_reservation_summary_callback');
+         
