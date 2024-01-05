af1b90511857107ff679ff32485060b74df41fb7
Anabel Muñoz <anabel@mybooking.es>
2024-01-05 17:35:47 +0100

    Fixed pickup_place simple and rental location error in completeview - pending shooping cart duplication

--- mybooking-templates/mybooking-plugin-reservation-complete-summary-tmpl.php
+++ mybooking-templates/mybooking-plugin-reservation-complete-summary-tmpl.php
@@ -14,23 +14,38 @@
     <% if ( shopping_cart.pickup_place_customer_translation !== shopping_cart.return_place_customer_translation) { %>
       <div class="reservation-summary-sticky complete-sticky">
         <div class="sandwitch-wrapper">
-            <% if (configuration.pickupReturnPlace) { %>
+          <% if ( shopping_cart.rental_location_name ||
+                      shopping_cart.pickup_place_customer_translation ||
+                      shopping_cart.simple_location_name  
+                    )  { %>
             <div class="reservation-summary_pickup_place">
-              <span class="overflow-ellipsis"><%=shopping_cart.pickup_place_customer_translation%></span>
+              <span class="overflow-ellipsis">
+                <% if ( shopping_cart.rental_location_name ) { %>
+                  <%=shopping_cart.rental_location_name%>
+                <% } %>
+                <% if ( shopping_cart.pickup_place_customer_translation ) { %>
+                  <%=shopping_cart.pickup_place_customer_translation%>
+                <% } %>
+                <% if ( shopping_cart.simple_location_name ) { %>
+                  <%=shopping_cart.simple_location_name%>
+                <% } %>
+              </span>
             </div>
-            <% } else { %>
+          <% } else { %>
             <div class="ml-1"></div>
-            }
-            <% } %>
+          <% } %>
           <div class="reservation-summary_pickup_date">
-            <span><%=shopping_cart.date_from_short_format%>
-            <% if (configuration.rentDateSelector === 'date_from_date_to') { %>
-              <% if (configuration.timeToFrom) { %><%=shopping_cart.time_from%><%}%>
-            <% } %>
+            <span>
+              <%=shopping_cart.date_from_short_format%>
+              <% if (configuration.rentDateSelector === 'date_from_date_to') { %>
+                <% if (configuration.timeToFrom) { %><%=shopping_cart.time_from%><%}%>
+              <% } %>
             </span>
           </div>
         </div>
+
         <div class="separator"></div>
+
         <div class="sandwitch-wrapper push-to-the-right">
           <div class="reservation-summary_return_place">
             <span class="overflow-ellipsis">
@@ -39,32 +54,35 @@
           </div>
           <div class="reservation-summary_return_date">
             <% if (configuration.rentDateSelector === 'date_from_duration') { %>
-              <span><%=shopping_cart.renting_duration_literal%>
-                  <% if (!shopping_cart.renting_duration_literal) { %>
-                    <% if ( (typeof shopping_cart.half_day !== 'undefined' && shopping_cart.half_day) || (shopping_cart.days == 0) ) { %>
-                      <% if ( typeof shopping_cart.turn_description !== 'undefined' && shopping_cart.turn_description !== null && shopping_cart.turn_description !== '') { %>
-                        - <%= shopping_cart.turn_description %> 
-                      <% } else { %>
-                        ( <%= shopping_cart.time_from %> - <%= shopping_cart.time_to %> )
-                      <% } %>
+              <span>
+                <%=shopping_cart.renting_duration_literal%>
+                <% if (!shopping_cart.renting_duration_literal) { %>
+                  <% if ( (typeof shopping_cart.half_day !== 'undefined' && shopping_cart.half_day) || (shopping_cart.days == 0) ) { %>
+                    <% if ( typeof shopping_cart.turn_description !== 'undefined' && shopping_cart.turn_description !== null && shopping_cart.turn_description !== '') { %>
+                      - <%= shopping_cart.turn_description %> 
+                    <% } else { %>
+                      ( <%= shopping_cart.time_from %> - <%= shopping_cart.time_to %> )
                     <% } %>
                   <% } %>
+                <% } %>
               </span>
             <% } else { %>
-              <span><%=shopping_cart.date_to_short_format%>
-              <% if (configuration.timeToFrom) { %><%=shopping_cart.time_to%><%}%></span>
+              <span>
+                <%=shopping_cart.date_to_short_format%>
+                <% if (configuration.timeToFrom) { %><%=shopping_cart.time_to%><%}%>
+              </span>
             <% } %>
           </div>
         </div>
         
         <!-- // Row for price & buttons  -->
         <div class="complete-summary-row">
-          <div class="complete-buttons-wrapper">
-          </div>
+          <div class="complete-buttons-wrapper"></div>
 
           <div class="complete-summary-price-wrapper">
             <div class="complete-summary-total-title">
-              <?php echo esc_html_x( "Total", 'renting_complete', 'mybooking' ) ?></div>
+              <?php echo esc_html_x( "Total", 'renting_complete', 'mybooking' ) ?>
+            </div>
             <div class="complete-summary-total-price">
               <%=configuration.formatCurrency(shopping_cart.total_cost)%>
             </div>
@@ -76,9 +94,22 @@
     <% } else { %>
 
       <div class="reservation-summary-sticky">
-        <% if (configuration.pickupReturnPlace) { %>
+        <% if ( shopping_cart.rental_location_name ||
+                    shopping_cart.pickup_place_customer_translation ||
+                    shopping_cart.simple_location_name  
+                  )  { %>
           <div class="reservation-summary_pickup_place">
-            <span class="overflow-ellipsis"><%=shopping_cart.pickup_place_customer_translation%></span>
+            <span class="overflow-ellipsis">
+              <% if ( shopping_cart.rental_location_name ) { %>
+                <%=shopping_cart.rental_location_name%>
+              <% } %>
+              <% if ( shopping_cart.pickup_place_customer_translation ) { %>
+                <%=shopping_cart.pickup_place_customer_translation%>
+              <% } %>
+              <% if ( shopping_cart.simple_location_name ) { %>
+                <%=shopping_cart.simple_location_name%>
+              <% } %>
+            </span>
           </div>
         <% } %>
         <div class="reservation-summary_pickup_date">
